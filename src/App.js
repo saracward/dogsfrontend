894f22ccf431cdc77ed86ed7cf6b4b2d6e1e95ab
Sara Ward <saraward@MacBook-Pro-6.local>
2020-10-20 12:58:08 -0400

    thank goodness it works

--- src/App.js
+++ src/App.js
@@ -5,25 +5,105 @@ import Display from "./Display";
 import Form from "./Form";
 
 function App() {
+  const url = "https://dogsbackend.herokuapp.com";
+  //state to hold dogs
+  const [dogs, setDogs] = React.useState([]);
+  //empty dog for form
+  const emptyDog = {
+    name: "",
+    age: 0,
+    img: "",
+  };
+  //select a dog to update
+  const [selectedDog, setSelectedDog] = React.useState(emptyDog);
+
+  //function to fetch dogs
+  const getDogs = () => {
+    fetch(url + "/dog/")
+      .then((response) => response.json())
+      .then((data) => {
+        setDogs(data);
+      });
+  };
+  //get dogs on page load
+  React.useEffect(() => getDogs(), []);
+  //handleCreate Function for creating dogs
+  const handleCreate = (newDog) => {
+    fetch(url + "/dog/", {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newDog),
+    }).then((response) => getDogs());
+  };
+
+  //handleUpdate to update a dog when form is clicked
+  const handleUpdate = (dog) => {
+    fetch(url + "/dog/" + dog._id, {
+      method: "put",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(dog),
+    }).then((response) => getDogs());
+  };
+
+  //selectDog which selects a dog
+  const selectDog = (dog) => {
+    setSelectedDog(dog);
+  };
+
+  //to delete a dog
+  const deleteDog = (dog) => {
+    fetch(url + "/dog/" + dog._id, {
+      method: "delete",
+    }).then((response) => getDogs());
+  };
+
   return (
     <div className="App">
       <h1>DOG LISTING SITE</h1>
       <hr />
+      <Link to="/create">
+        <button>Add Dog</button>
+      </Link>
       <main>
         <Switch>
-          <Route exact path="/" render={(rp) => <Display {...rp} />} />
+          <Route
+            exact
+            path="/"
+            render={(rp) => (
+              <Display
+                {...rp}
+                dogs={dogs}
+                selectDog={selectDog}
+                deleteDog={deleteDog}
+              />
+            )}
+          />
           <Route
             exact
             path="/create"
             render={(rp) => (
-              <Form {...rp} label="create" dog={{}} handleSubmit={() => {}} />
+              <Form
+                {...rp}
+                label="create"
+                dog={emptyDog}
+                handleSubmit={handleCreate}
+              />
             )}
           />
           <Route
             exact
             path="/edit"
             render={(rp) => (
-              <Form {...rp} label="update" dog={{}} handleSubmit={() => {}} />
+              <Form
+                {...rp}
+                label="update"
+                dog={selectedDog}
+                handleSubmit={handleUpdate}
+              />
             )}
           />
         </Switch>
--- src/Display.js
+++ src/Display.js
@@ -1,7 +1,37 @@
 import React from "react";
 
 const Display = (props) => {
-  return <h1>Display</h1>;
+  const { dogs } = props;
+  const loaded = () => (
+    <div style={{ textAlign: "center" }}>
+      {dogs.map((dog) => (
+        <article>
+          <img src={dog.img} />
+          <h1>{dog.name}</h1>
+          <h3>{dog.age}</h3>
+          <button
+            onClick={() => {
+              props.selectDog(dog);
+              props.history.push("/edit");
+            }}
+          >
+            Edit
+          </button>
+          <br />
+          <button
+            onClick={() => {
+              props.deleteDog(dog);
+            }}
+          >
+            Delete
+          </button>
+        </article>
+      ))}
+    </div>
+  );
+
+  //display loaded if there are dogs, show loading if there isn't
+  return dogs.length > 0 ? loaded() : <h1>loading</h1>;
 };
 
 export default Display;
